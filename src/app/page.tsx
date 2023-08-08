b57b0a1ed7f25a21d86557f4b9fb5cdce30bff74
SWH-Relewise <swh@relewise.com>
2023-08-08 14:46:19 +0200

    handle react/no-unescaped-entities compile error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
           </h1>
 
           <p className="pb-2">
-            Discover a wide range of offerings with our search and discovery tools, and take advantage of personalized product recommendations. Our platform provides a powerful search experience and intelligent recommendations to help you find exactly what you're looking for. With our advanced technology, exploring and discovering new products has never been easier.
+            Discover a wide range of offerings with our search and discovery tools, and take advantage of personalized product recommendations. Our platform provides a powerful search experience and intelligent recommendations to help you find exactly what you&apos;re looking for. With our advanced technology, exploring and discovering new products has never been easier.
           </p>
 
           <p>
-            Relewise is an intelligent personalization platform that provides customized and relevant online experiences, designed to empower both developers and marketers. Our advanced search and recommendation algorithms ensure that you always find what you're looking for, and discover products you'll love.
+            Relewise is an intelligent personalization platform that provides customized and relevant online experiences, designed to empower both developers and marketers. Our advanced search and recommendation algorithms ensure that you always find what you&apos;re looking for, and discover products you&apos;ll love.
           </p>
         </div>
       </div>
