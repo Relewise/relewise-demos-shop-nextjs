7d97d91c1404eb60c8f1afffe8db2468b9c9910b
SWH-Relewise <swh@relewise.com>
2023-08-14 10:07:38 +0200

    ensure correct production url

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 
+const ON_GITHUB_PAGES = process.env.NODE_ENV === "production";
+
+const repoName = "relewise-demos-shop-nextjs";
+
+const basePath = ON_GITHUB_PAGES ? `/${repoName}` : "";
+const assetPrefix = ON_GITHUB_PAGES ? `/${repoName}/` : "";
+
 const nextConfig = {
   images: {
     unoptimized: true,
     domains: ["relewise.com"]
   },
-  output: "export"
+  output: "export",
+  basePath: basePath,
+  assetPrefix: assetPrefix
 };
 
 module.exports = nextConfig;
